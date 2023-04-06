4a7800c1cf5cca09a51ca6b52ac43ccda506ab64
SeaonLeague <facebookups23@gmail.com>
2023-04-05 18:50:42 -0700

    initial commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,12 +4,13 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
+rm -rf /data/web_static/current
+
 sudo echo "<html>
   <head>
   </head>
@@ -25,3 +26,4 @@ sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
+exit 0
